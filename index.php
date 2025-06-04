675cc9cc2176d7343a640f43fde763368f9e5977
DESKTOP-3VURKDF\Mihailo <hkjh>
2025-06-04 20:31:41 +0200

    Ovo je neka poruka

--- index.php
+++ index.php
@@ -8,49 +8,42 @@
 
 <?php
 
+echo "Zdravo svete!<br>";
 $a=5;
 $b=10;
-echo "Ja pisem $a";
-$a="tekst";
-?>
+echo $a+$b;
+$a="<br>ovo je tekst ";
+$b="i ovo je neki tekst";
+echo $a.$b." ovo je tekst<br>";
 
-<br>
-<h1>ovo je naslov</h1>
-<p>ovo je paragraf</p>
-
-<?php
-$x=5;
+$x=12.2345;
 
 function Test()
 {
-    static $h=0;
-    $h=$h+1;
-    echo "<br>prom h unutar fje Test je ".$h;
+    global $x;
+    static $n=0;
+    
+    $n=$n+1;
+    echo "Promenjiva x unutar fje je $n<br>";
 }
 
+echo "Promenjiva x van fje je $x<br>";
 Test();
 Test();
 Test();
 Test();
-echo "<br>promenjiva x van fje Test je $x";
+var_dump($x);
+$niz=array("mika","pera","zika",123,64.45235);
+var_dump($niz);
+define("NASLOV","Dobrodošli!");
+$NASLOV=123;
+echo $NASLOV." ".NASLOV;
 
-$p="fasd";
-echo "<br>";
-var_dump($p);
-
-class Car
+foreach($niz as $i)
 {
-	function Car()
-	{
-		$this->model="";
-	}
+    echo "<br>".$i."<br>";
 }
-$auto=new Car(); //kreiranje objekta
-$auto->model="Volvo";
-var_dump($auto);
-//echo $auto->model;
-$ucenici=array("Milan","Marko","Jovan","Ivana",12,45.45);
-var_dump($ucenici);
+
 
 ?>
 
