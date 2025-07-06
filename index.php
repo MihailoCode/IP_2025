3f883e6a1d9e6c1b8ef84aec7f315b39a7edc997
DESKTOP-3VURKDF\Mihailo <hkjh>
2025-07-06 18:12:25 +0200

    Dodata grana

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>CRUD</title>
+</head>
+<body>
+<header><?php include './templates/header.php';?></header>
+<nav><?php include './templates/nav.php';?></nav>
+	<form action="controllerOsobe.php" method="post">
+		Ime: <br>
+		<input type="text" name="ime"><br>
+		Prezime: <br>
+		<input type="text" name="prezime"><br>
+		Godiste: <br>
+		<input type="text" name="godiste"><br>
+		<input type="submit" name="action" value="Unesi">
+	</form>
+<footer><?php include './templates/footer.php';?></footer>
+</body>
+</html>
